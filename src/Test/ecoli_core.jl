bdf960e1d4e034703344f42199d7b78f71ecaccb
josePereiro <jose.pereiro.lab@gmail.com>
2021-10-17 00:49:45 -0400

    Remove show

--- src/Test/ecoli_core.jl
+++ src/Test/ecoli_core.jl
@@ -2,7 +2,6 @@ const ECOLI_MODEL_BIOMASS_IDER = "Biomass_Ecoli_core_w_GAM"
 
 function ecoli_core_model()
     matfile = joinpath(@__DIR__, "..", "..", "data", "ecoli_core_model.mat")
-    @show matfile
     mat_model = MAT.matread(matfile)["model"]
     return MetNet(mat_model; reshape = true)
 end
