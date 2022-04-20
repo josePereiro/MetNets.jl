003b76f6e8f78668049311cfd4cd695101a88594
josePereiro <jose.pereiro.lab@gmail.com>
2022-04-20 01:12:51 -0400

    Up deps

--- src/Test/ecoli_core.jl
+++ src/Test/ecoli_core.jl
@@ -1,7 +1,7 @@
 const ECOLI_MODEL_BIOMASS_IDER = "Biomass_Ecoli_core_w_GAM"
+const ECOLI_MODEL_MAT_FILE = abspath(joinpath(@__DIR__, "..", "..", "data", "ecoli_core_model.mat"))
 
 function ecoli_core_model()
-    matfile = joinpath(@__DIR__, "..", "..", "data", "ecoli_core_model.mat")
-    mat_model = MAT.matread(matfile)["model"]
+    mat_model = MAT.matread(ECOLI_MODEL_MAT_FILE)["model"]
     return MetNet(mat_model; reshape = true)
 end
